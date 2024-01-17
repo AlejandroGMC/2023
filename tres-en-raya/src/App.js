1481d3101358915d59d862ca6b4a449e0503199c
AlejandroGMC <155449759+AlejandroGMC@users.noreply.github.com>
2024-01-17 15:49:12 +0000

    semana3

--- tres-en-raya/src/App.js
+++ tres-en-raya/src/App.js
@@ -3,19 +3,34 @@ import './App.css';
 import Tablero from './components/Tablero';
 import TableroB from './components/TableroB';
 import TableroC from './components/TableroC';
+import TableroD from './components/TableroD';
+import TableroE from './components/TableroE';
 import Juego from './components/Juego';
+import JuegoA from './components/JuegoA';
+import CuadroB from './components/CuadroB';
 
 function App() {
 
+  function TableroE({ cuadros, onclick }) {
+    function renderizarCuadro(i){
+      return (
+        <CuadroB
+        valor={i}
+        funcion={()=> onclick(i)}
+        />
+      );
+    }
+  }
 
 
   return (
     <div className="App">
 
-      {<Tablero />
-      /*<TableroB />
-      <TableroC /> */}
-      <Juego />
+    {/*Tablero />
+      </TableroB />
+      <TableroE />
+  <Juego />*/}
+      <JuegoA />
     </div>
   );
 }
